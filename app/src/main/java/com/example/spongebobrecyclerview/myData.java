1b218b36ec29ab47f86a987a43e334fe323eedc0
Tamir Razon <tamir.razon@gmail.com>
2024-02-19 00:56:46 +0200

    Fix some issues with the images

--- app/src/main/java/com/example/spongebobrecyclerview/myData.java
+++ app/src/main/java/com/example/spongebobrecyclerview/myData.java
@@ -4,9 +4,9 @@ public class myData {
     static String[] nameArray = {"SpongeBob", "Patrick", "Squidward", "Sandy", "Mr. Krabs", "Plankton", "Gary the Snail","Mrs. Puff", "Pearl Krabs", "Karen Plankton"};
     static String[] versionArray = {"1.5", "1.6", "2.0-2.1", "2.2-2.2.3", "2.3-2.3.7", "3.0-3.2.6", "4.0-4.0.4", "4.1-4.3.1", "4.4-4.4.4","6.0-6.0.1"};
 
-    static Integer[] drawableArray = {R.drawable.Spongebob, R.drawable.Patrick, R.drawable.Squidward,
-            R.drawable.Sandy, R.drawable.Mr_Krabs, R.drawable.Plankton,
-            R.drawable.Gary, R.drawable.Mrs_Puff, R.drawable.Pearl,R.drawable.Karen};
+    static Integer[] drawableArray = {R.drawable.spongebob, R.drawable.patrick, R.drawable.squidward,
+            R.drawable.sandy, R.drawable.mr_krabs, R.drawable.plankton,
+            R.drawable.gary, R.drawable.mrs_puff, R.drawable.pearl,R.drawable.karen};
 
     static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 }
